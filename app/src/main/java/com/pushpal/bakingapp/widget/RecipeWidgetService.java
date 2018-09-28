1fed6397c2e5d154edc66f98d376e3cff1fe71ca
Pushpal <pushpalroy2007@gmail.com>
2018-09-29 00:54:31 +0530

    Stopped database operations running on a separate thread. Operations in onDataSetChanged() should be performed before getViewAt() is executed.

--- app/src/main/java/com/pushpal/bakingapp/widget/RecipeWidgetService.java
+++ app/src/main/java/com/pushpal/bakingapp/widget/RecipeWidgetService.java
@@ -2,13 +2,10 @@ package com.pushpal.bakingapp.widget;
 
 import android.content.Context;
 import android.content.Intent;
-import android.database.sqlite.SQLiteConstraintException;
-import android.util.Log;
 import android.widget.RemoteViews;
 import android.widget.RemoteViewsService;
 
 import com.pushpal.bakingapp.R;
-import com.pushpal.bakingapp.database.AppExecutors;
 import com.pushpal.bakingapp.database.IngredientDatabase;
 import com.pushpal.bakingapp.database.RecipeIngredient;
 
@@ -39,30 +36,21 @@ class RecipeRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory 
     }
 
     private void populateListItem() {
-        AppExecutors.getInstance().diskIO().execute(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    IngredientDatabase db = IngredientDatabase.getInstance(context);
-                    List<RecipeIngredient> ingredients = db.ingredientDao().getAllIngredients();
-                    for (RecipeIngredient ingredient : ingredients) {
-                        ListItem listItem = new ListItem();
-                        listItem.ingredient = ingredient.getIngredientName();
-                        listItem.measurement = ingredient.getIngredientMeasure();
+        listItemList.clear();
+        IngredientDatabase db = IngredientDatabase.getInstance(context);
+        List<RecipeIngredient> ingredients = db.ingredientDao().getAllIngredients();
+        for (RecipeIngredient ingredient : ingredients) {
+            ListItem listItem = new ListItem();
+            listItem.ingredient = ingredient.getIngredientName();
+            listItem.measurement = ingredient.getIngredientMeasure();
 
-                        listItemList.add(listItem);
-                    }
-                } catch (SQLiteConstraintException e) {
-                    Log.e(TAG, e.getMessage());
-                }
-            }
-        });
+            listItemList.add(listItem);
+        }
     }
 
     @Override
     public RemoteViews getViewAt(int position) {
-        final RemoteViews remoteView = new RemoteViews(
-                context.getPackageName(), R.layout.list_row);
+        final RemoteViews remoteView = new RemoteViews(context.getPackageName(), R.layout.list_row);
         ListItem listItem = listItemList.get(position);
         remoteView.setTextViewText(R.id.tv_ingredient, listItem.ingredient);
         remoteView.setTextViewText(R.id.tv_measure, listItem.measurement);
@@ -97,7 +85,9 @@ class RecipeRemoteViewsFactory implements RemoteViewsService.RemoteViewsFactory 
 
     @Override
     public void onCreate() {
-
+        /* In onCreate() you setup any connections / cursors to your data source. Heavy lifting,
+        for example downloading or creating content etc, should be deferred to onDataSetChanged()
+        or getViewAt(). Taking more than 20 seconds in this call will result in an ANR. */
     }
 
     @Override
